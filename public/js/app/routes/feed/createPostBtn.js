f61be155d3d5ea7c73684372d5d3d706b4b04a34
telecasteren <127958662+telecasteren@users.noreply.github.com>
2025-03-27 22:24:41 +0100

    Minor cleanup in creating posts

--- public/js/app/routes/feed/createPostBtn.js
+++ public/js/app/routes/feed/createPostBtn.js
@@ -4,8 +4,8 @@ export default function createPostBtn() {
 
   const menu = document.createElement("div");
   menu.id = "speed-dial-menu-dropdown";
-  menu.className =
-    "flex flex-col justify-end hidden py-1 mb-4 bg-white border border-gray-100 rounded-lg shadow-xs dark:bg-gray-700 dark:border-gray-600";
+  menu.className = `flex flex-col justify-end hidden py-1 mb-4 bg-white
+  border border-gray-100 rounded-lg shadow-xs dark:bg-gray-700 dark:border-gray-600`;
 
   const ul = document.createElement("ul");
   ul.className = "text-sm text-gray-500 dark:text-gray-300";
@@ -13,21 +13,9 @@ export default function createPostBtn() {
   const li = document.createElement("li");
   const a = document.createElement("a");
   a.href = "#";
-  a.className =
-    "flex items-center px-5 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-gray-900 dark:hover:text-white";
+  a.className = `flex items-center px-5 py-2 bg-white border border-gray-200 rounded-md shadow-sm hover:text-gray-900
+  hover:bg-gray-100 dark:bg-[#0f0c29] dark:hover:bg-gray-600 dark:hover:text-white`;
 
-  const svg = document.createElement("svg");
-  svg.className = "w-3.5 h-3.5 me-2";
-  svg.setAttribute("aria-hidden", "true");
-  svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-  svg.setAttribute("fill", "currentColor");
-  svg.setAttribute("viewBox", "0 0 20 18");
-
-  const path = document.createElement("path");
-  path.setAttribute("d", "M18 0H2a2 2...");
-
-  svg.appendChild(path);
-  a.appendChild(svg);
   a.appendChild(document.createTextNode("New post"));
   li.appendChild(a);
   ul.appendChild(li);
@@ -38,23 +26,14 @@ export default function createPostBtn() {
   button.type = "button";
   button.id = "create-post";
   button.setAttribute("aria-controls", "speed-dial-menu-dropdown");
-  button.className =
-    "flex items-center justify-center ml-auto text-white bg-blue-700 rounded-full w-14 h-14 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800";
+  button.className = `flex items-center justify-center text-center ml-auto
+  text-white bg-blue-700 rounded-full w-16 h-16 hover:bg-blue-800
+  focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`;
 
-  const svgButton = document.createElement("svg");
-  svgButton.className = "w-6 h-6";
-  svgButton.setAttribute("aria-hidden", "true");
-  svgButton.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-  svgButton.setAttribute("fill", "currentColor");
-  svgButton.setAttribute("viewBox", "0 0 20 20");
-
-  const pathButton = document.createElement("path");
-  pathButton.setAttribute(
-    "d",
-    "m13.835 7.578-.005.007-7.137 7.137 2.139 2.138 7.143-7.142-2.14-2.14Z..."
-  );
-  svgButton.appendChild(pathButton);
-  button.appendChild(svgButton);
+  const buttonIcon = document.createElement("p");
+  buttonIcon.className = "font-medium text-bigger text-center";
+  buttonIcon.innerText = "+";
+  button.appendChild(buttonIcon);
 
   const span = document.createElement("span");
   span.className = "sr-only";
